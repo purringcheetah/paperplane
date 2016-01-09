2b3c23b0fcf6190e2025f8e5a0d729001d5da534
Remi Gillig <remigillig@gmail.com>
2016-01-09 10:09:27 +0100

    Copy of original site (missing downloads for now)

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>PaperPlane - The Game</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.lightbox-0.5.pack.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" media="screen" />
+    <link rel='shortcut icon' href='/images/favicon.ico' />
+    <script type="text/javascript">
+    $(function() {
+        $('#gallery .screens a').lightBox();
+    });
+    </script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-12141305-3']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+</head>
+<body>
+    <div id="bg">
+        <div id="page">
+            <div id="header">
+                <h1>PaperPlane</h1>
+                <span class="logo"><img src="images/header.png"/></span>
+            </div>
+            <div id="menu">
+                <div id="menu-header"></div>
+                <div id="menu-content">
+                    <ul>
+                        <li><a class="overview" href="#overview"><span>Overview</span></a></li>
+                        <li><a class="screenshots" href="#screenshots"><span>Screenshots</span></a></li>
+                        <li><a class="video" href="#video"><span>Video</span></a></li>
+                        <li><a class="download" href="#download"><span>Download</span></a></li>
+                        <li><a class="team" href="#team"><span>Team</span></a></li>
+                    <ul>
+                </div>
+                <div id="menu-footer"></div>
+            </div>
+            <div id="content">
+                <h1><a id="overview"><img src="images/title-overview.png"/><span>Overview</span></a></h1>
+                <p>
+                    PaperPlane is a video game where you fly a paper airplane through typical childhood memories. You give life back to these memories
+                    by flying close to them, through them or around them.
+                </p>
+                <p>
+                    Enabling those memories give access to little sketches which put some movement to the landscape. Also, each one of them is can be viewed
+                    in a polaroid pictures book inside the tree house.
+                </p>
+                <p>
+                    PaperPlane won the student first prize (ex-aequo) at the Milthon Awards 2010.
+                </p>
+                <p>
+                    PaperPlane got the <a href="http://igf.com/2011/01/2011_independent_games_festiva_12.html">IGF 2011 Student Showcase</a> prize.
+                </p>
+                <p>
+                    <b>Update:</b> <a href="./#download">new version 2011-01-14-3 available</a>!
+                </p>
+                <h1><a id="screenshots"><img src="images/title-screenshots.png"/><span>Screenshots</span></a></h1>
+                <div id="gallery">
+                    <table class="screens">
+                        <tr>
+                            <td><a href="screenshots/01_small.jpg"><img src="screenshots/01_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/02_small.jpg"><img src="screenshots/02_thumb.jpg"/></a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="screenshots/03_small.jpg"><img src="screenshots/03_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/04_small.jpg"><img src="screenshots/04_thumb.jpg"/></a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="screenshots/05_small.jpg"><img src="screenshots/05_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/06_small.jpg"><img src="screenshots/06_thumb.jpg"/></a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="screenshots/07_small.jpg"><img src="screenshots/07_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/08_small.jpg"><img src="screenshots/08_thumb.jpg"/></a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="screenshots/09_small.jpg"><img src="screenshots/09_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/10_small.jpg"><img src="screenshots/10_thumb.jpg"/></a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="screenshots/11_small.jpg"><img src="screenshots/11_thumb.jpg"/></a></td>
+                            <td><a href="screenshots/12_small.jpg"><img src="screenshots/12_thumb.jpg"/></a></td>
+                        </tr>
+                    </table>
+                </div>
+                <h1><a id="video"><img src="images/title-video.png"/><span>Video</span></a></h1>
+                <p>
+                    <object width="640" height="385"><param name="movie" value="http://www.youtube.com/v/jof_1ByCtKM?fs=1&amp;hl=en_US&amp;rel=0&amp;hd=1&amp;color1=0xe3c190&amp;color2=0xe6cd92"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/jof_1ByCtKM?fs=1&amp;hl=en_US&amp;rel=0&amp;hd=1&amp;color1=0xe3c190&amp;color2=0xe6cd92" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="385"></embed></object>
+                </p>
+                <h1><a id="download"><img src="images/title-download.png"/><span>Download</span></a></h1>
+                <p>
+                    <p>
+                        What you need :
+                        <ul>
+                            <li>Windows XP (SP2 or later), Vista or Seven</li>
+                            <li>Graphics Card with Shader Model 2.0</li>
+                            <li>Around 300MB on your hard drive</li>
+                            <li>Keyboard / Xbox 360 Controller (optional)</li>
+                        </ul>
+                        
+                        Included in the setup :
+                        <ul>
+                            <li>PaperPlane</li>
+                            <li>DirectX (August 2009) Redist</li>
+                            <li>Visual Studio 2008 Redist</li>
+                        </ul>
+                        
+                        What's new (in version 2011-01-14-3) :
+                        <ul>
+                            <li>Keyboard controls (by default if you don't have a gamepad)</li>
+                            <li>Some little bugs fixes</li>
+                            <li>Changed the intro screens</li>
+                        </ul>
+                        
+                        What's new (in version 2014-10-01) :
+                        <ul>
+                            <li>Fix crash in Unity</li>
+                            <li>Removed intro video for smaller size</li>
+                            <li>Zip file instead of setup</li>
+                        </ul>
+                    </p>
+                    
+                    <!--<p><span class="download"><a href="http://paperplane-game.com/pub/paperplane-setup-2010-11-18-2.exe">http://paperplane-game.com/pub/paperplane-setup-2010-11-18-2.exe</a></span></p>-->
+                    <!--<p><span class="download"><a href="http://paperplane-game.com/pub/paperplane-setup-2011-01-14-3.exe">http://paperplane-game.com/pub/paperplane-setup-2011-01-14-3.exe (200 MB)</a></span></p>-->
+                    <p><span class="download"><a href="http://paperplane-game.com/pub/paperplane-2014-10-01.zip">http://paperplane-game.com/pub/paperplane-2014-10-01.zip (116 MB)</a></span></p>
+                    
+                    <p>
+                        Extras :
+                        <ul>
+                            <li><span class="download"><a href="http://paperplane-game.com/pub/paperplane-theme.mp3">Music theme (MP3, 5MB)</a></span></li>
+                            <li><span class="download"><a href="http://paperplane-game.com/pub/paperplane-screens-hd.zip">Screens from above (1280x720, 9 MB)</a></span></li>
+                        </ul>
+                    </p>
+                </p>
+                <h1><a id="team"><img src="images/title-team.png"/><span>Team</span></a></h1>
+                <p>
+                    <table class="team">
+                        <tr><td class="name">Martial Potron</td><td>Producer</td></tr>
+                        <tr><td class="name">K&eacute;vin Poupard</td><td>Game Designer</td></tr>
+                        <tr><td class="name">Pierre Hornez</td><td>Game Designer</td></tr>
+                        <tr><td class="name">Pierre Lemasson</td><td>Graphics Designer</td></tr>
+                        <tr><td class="name">Ronan Kergosien</td><td>Graphics Designer</td></tr>
+                        <tr><td class="name">R&eacute;mi Gillig</td><td>Programmer</td></tr>
+                        <tr><td class="name">Vincent Pancaldi</td><td>Programmer</td></tr>
+                        <tr><td class="name">Laurent Martin</td><td>Sound Designer</td></tr>
+                        <tr><td class="name">Julien Barbe</td><td>Usability Expert</td></tr>
+                    </table>
+                    To contact us, please e-mail us at <a href="mailto:paperplane@speps.fr">paperplane@speps.fr</a>. We love to hear from you all!
+                </p>
+            </div>
+            <div id="footer">&copy; Copyright <?php echo date("Y"); ?> - All Rights Reserved - <a href="mailto:paperplane@speps.fr">PaperPlane Team</a></div>
+        </div>
+    </div>
+</body>
+</html>
+
